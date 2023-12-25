7e61cbd7a6fc84ac82b108c207abbbcdfe9d53c3
nikita-hash <63429718+nikita-hash@users.noreply.github.com>
2023-12-25 16:17:02 +0300

    aasd

--- authentication/src/main/java/api/v1/authentication/config/PdfProducer.java
+++ authentication/src/main/java/api/v1/authentication/config/PdfProducer.java
@@ -1,7 +1,7 @@
 package api.v1.authentication.config;
 
 import api.v1.authentication.dto.PdfEvent;
-import api.v1.authentication.persistency.User;
+
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.slf4j.Logger;
@@ -11,7 +11,6 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -19,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class PdfProducer {
 
     private static final Logger LOGGER= LoggerFactory.getLogger(PdfProducer.class);
+    private final String TOPIC="pdf_topic";
 
     @Autowired
     public PdfProducer(NewTopic topic, KafkaTemplate<String, PdfEvent> kafkaTemplate) {
@@ -31,13 +31,10 @@ public class PdfProducer {
     private KafkaTemplate<String, PdfEvent> kafkaTemplate;
 
 
-
     public void sendMessage(PdfEvent pdfEvent){
+
         LOGGER.info(String.format("Доставка сообщения от пользователя : ",pdfEvent.getUser().getName()));
-        Message<?>message=MessageBuilder.withPayload(pdfEvent)
-                .setHeader(KafkaHeaders.TOPIC,"pdf_topic")
-                .build();
-        kafkaTemplate.send(message);
+        kafkaTemplate.send(TOPIC,String.valueOf(pdfEvent.getStatus()),pdfEvent);
     }
 
 
--- authentication/src/main/java/api/v1/authentication/controller/AuthenticationController.java
+++ authentication/src/main/java/api/v1/authentication/controller/AuthenticationController.java
@@ -1,18 +1,19 @@
 package api.v1.authentication.controller;
-
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import api.v1.authentication.config.PdfProducer;
+import api.v1.authentication.dto.PdfEvent;
 import api.v1.authentication.persistency.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import api.v1.authentication.dto.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,16 +25,10 @@ public class AuthenticationController {
     @PostMapping("/registration")
     public ResponseEntity<?> registration(@RequestBody @Validated User user,
                                           BindingResult bindingResult) {
-        User user1=new User();
-        user1.setEmail("Email");
-        user1.setName("Никита");
-        user1.setPassword("Password");
-        producer.sendMessage(
-                PdfEvent
-                        .builder()
-                        .message("Создать файл")
-                        .user(user1)
-                        .status(10).build());
+        if(bindingResult.hasErrors()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getAllErrors());
+        }
+        else
         return ResponseEntity.status(HttpStatus.OK).body("Успешно зарегестрирован !");
     }
 
@@ -43,7 +38,12 @@ public class AuthenticationController {
     }
 
     @GetMapping("/create_pdf")
-    public ResponseEntity<?> getfile() {
-        return ResponseEntity.status(HttpStatus.OK).body(new byte[200]);
+    public ResponseEntity<?> getfile(@RequestBody User user) {
+        PdfEvent pdfEvent=new PdfEvent();
+        pdfEvent.setMessage("Привет мир");
+        pdfEvent.setStatus(10);
+        pdfEvent.setUser(user);
+        producer.sendMessage(pdfEvent);
+        return ResponseEntity.status(HttpStatus.OK).body("Успешно");
     }
 }
